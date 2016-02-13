a68291e5d0132a964a17f8816bc1ccb0d5af5c89
xhxhx <angelhxh90@gmail.com>
2016-02-13 10:40:33 +0800

    FEWD_Week8

---
+++ week8_working_files/form_style_validation/js/validate.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('#registerForm').parsley().on('field:validated', function() {
+    var ok = $('.parsley-error').length === 0;
+    // $('.bs-callout-info').toggleClass('hidden', !ok);
+    // $('.bs-callout-warning').toggleClass('hidden', ok);
+  })
+  .on('form:submit', function() {
+    return false; // Don't submit form for this demo
+  });
+});
